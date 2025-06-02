bd7722e80912a4c6a09ac2a363346d011a518bd9
sulemanbutta <sulemanbutta98@gmail.com>
2025-06-01 20:32:05 -0500

    Revert "auth traffice update 5"
    This reverts commit dfbefe8856c4b1147d2e332ec13495f8d34f5e57.

--- services/auth-service/src/models/index.js
+++ services/auth-service/src/models/index.js
@@ -9,16 +9,13 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.json')[env];
 const db = {};
 
-const sequelize = require("../services/db"); 
-console.log("▶️ [models/index.js] Using existing database connection");
-/*
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-*/
+
 fs
   .readdirSync(__dirname)
   .filter(file => {
